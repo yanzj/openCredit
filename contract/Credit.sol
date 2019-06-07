cc2344c130b73dd14c00017163356c224b0c9524
YoungWilliamZ <youngwilliam.zheng@gmail.com>
2019-06-07 10:42:31 +0800

    test pass in remix

--- contract/Credit.sol
+++ contract/Credit.sol
@@ -1,7 +1,12 @@
 pragma solidity >=0.4.0 <0.7.0;
-pragma experimental ABIEncoderV2;
 
 contract Credit {
+
+    // ?上次遗留：如果不 map到一个数组，如何读取历史记录,如何高效查找数据
+    // ？如果要修改一个数组的一个值，如何修改？ 直接取出来是放在内存里的，修改完之后会自动覆盖在数组上面吗？还是会重新建一个数组？
+    mapping(bytes32 => CreditData[]) private credits; // key = peopleId
+    bytes32[] private idArray;
+    uint private id; // 初始化为 0
 
     struct CreditData {
         uint id;               // index
@@ -11,28 +16,28 @@ contract Credit {
         uint time;             // time the data upload
     }
 
-    // ?上次遗留：如果不 map到一个数组，如何读取历史记录,如何高效查找数据
-    // ？如果要修改一个数组的一个值，如何修改？ 直接取出来是放在内存里的，修改完之后会自动覆盖在数组上面吗？还是会重新建一个数组？
-    mapping(bytes32 => CreditData[]) public credits; // key = id
-    bytes32[] public idArray;
-    uint id; // 初始化为 0
-
-    function addCreditData(string memory id_string, string memory data_string) public {
+    function addCreditData(string memory id_string, string memory data_string) public
+    returns (uint)
+    {
         id = id + 1;
         bytes32 peopleId = keccak256(abi.encodePacked(id_string));
-        CreditData memory credit = CreditData(id, msg.sender, peopleId, keccak256(abi.encodePacked(data_string)), now);
+        CreditData memory credit = CreditData(id, msg.sender, peopleId,
+                                              keccak256(abi.encodePacked(data_string)),
+                                              now);
         credits[peopleId].push(credit);
         idArray.push(peopleId);
+        return credit.id;
     }
 
     // 问题： 数据量太大怎么办? 先不考虑
-    function getCreditDetialDataById(string memory id_string) public view
+    function getCreditDetialDataByPeopleId(string memory id_string) public view
     returns(
             uint[] memory,    //  id
             address[] memory, // uploader, 返回为二进制，如何辨识
             //bytes32[] memory,
             bytes32[] memory,              // datas
-            uint[] memory) {                // times
+            uint[] memory)      // times
+    {
         CreditData[] memory credit_array = credits[keccak256(abi.encodePacked(id_string))];
         uint credit_array_length = credit_array.length;
         uint[] memory ids = new uint[](credit_array_length);
@@ -48,16 +53,5 @@ contract Credit {
             times[i] = credit_array[i].time;
         }
         return (ids, uploaders, datas, times);
-    }
-
-    function getCreditDataById(uint id) public view
-    returns(CreditData memory) {
-        for (uint i = 0; i < idArray.length; i++) {
-            for (uint j = 0; j < credits[idArray[i]].length ;j++) {
-                if (credits[idArray[i]][j].id == id){
-                    return credits[idArray[i]][j];
-                }
-            }
-        }
     }
 }
--- contract/Record.sol
+++ contract/Record.sol
@@ -1,86 +1,130 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 contract Record {
+    mapping(address => RecordData[]) private records; // key = applicant address
+    address[] private applicantArray;          // stores the organizes that applied the data
+    uint private id;
 
     struct RecordData {
         address applicant; // the organize want the data
+        address owner;     // the organize upload the data
         uint id;           // index
         uint creditDataId; // the id of credit data
         uint time;
         bool isSent;       // whether the original data has been sent from the uploader
-        uint8 score;        // the score of this record
+        uint8 score;       // the score of this record
         bool isScored;     // whether the uploader has scored
     }
-    mapping(address => RecordData[]) records; // key = applicant address
-    address[] public applicantArray;
-    uint id;
 
-    // 请求数据， 链上记录
-    function addRecordData(uint creditDataId) public payable returns (uint ) {
+    // Add Record Data
+    function addRecordData(uint creditDataId, address owner) public payable
+    returns (address, address, uint, uint, uint) {
+        require(owner != msg.sender, "The onwner of the data could not be the applicant!");
         id = id + 1;
-        RecordData memory record = RecordData(msg.sender, id, creditDataId, now, false, 0, false);
+        RecordData memory record = RecordData(msg.sender,
+                                              owner,
+                                              id,
+                                              creditDataId,
+                                              now,
+                                              false,
+                                              0,
+                                              false);
         records[msg.sender].push(record);
         applicantArray.push(msg.sender);
-        return id;
+        return (record.applicant, record.owner, record.id, record.creditDataId, record.time);
     }
 
-    // get serialized record data by id
-    function getRecordDataDetialById(uint id) public view returns (bytes memory) {
-        return extractRecordDataToBytes(getRecordDataById(id)); // return a zero record to imply can't find the record
+    // Check if the record is stored.
+    function checkRecordExist(address applicant,
+                                address owner,
+                                uint recordId,
+                                uint creditDataId) public view
+    returns (bool) {
+        for (uint j = 0; j < records[applicant].length; j++) {
+            if (records[applicant][j].applicant == applicant &&
+                records[applicant][j].owner == owner &&
+                records[applicant][j].id == recordId &&
+                records[applicant][j].creditDataId == creditDataId) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    function getRecordDataById(uint id) private view returns (RecordData memory) {
+    // Get the RecordData detail by recordId
+    function getRecordDataById(uint recordId) public view
+    returns (address, address, uint, uint, uint, bool, uint8, bool) {
         for (uint i = 0; i < applicantArray.length; i ++) {
             for (uint j = 0; j < records[applicantArray[i]].length; j++) {
-                if (records[applicantArray[i]][j].id == id) {
-                    return records[applicantArray[i]][j];
+                if (records[applicantArray[i]][j].id == recordId) {
+                    RecordData memory tmp = records[applicantArray[i]][j];
+                    return (tmp.applicant,
+                            tmp.owner,
+                            tmp.id,
+                            tmp.creditDataId,
+                            tmp.time,
+                            tmp.isSent,
+                            tmp.score,
+                            tmp.isScored);
                 }
             }
         }
-        RecordData memory record = RecordData(msg.sender, 0, 0, now, false, 0, false);
-        return record; // return a zero record to imply can't find the record
-    }
-
-    // 查询是否有请求
-    function getOneRecordData() public view returns(uint) {
-        for (uint i = 0; i < applicantArray.length; i++) {
-            for (uint j = 0; j < records[applicantArray[i]].length ;j++) {
-                if (records[applicantArray[i]][j].creditDataId == id){
-                    // Credit.credits[Credit.idArray[i]][j].isSent = true;
-                    return 1;
-                }
-            }
-        }
-        return 0;
+        return (msg.sender, msg.sender, 0, 0, now, false, 0, false);
+        // return a zero record to imply can't find the record
     }
 
     // 发送原始数据之后，将数据标为已发送
-    // 需要对 CreditData 进行查找, 时间与空间的如何选择？
-    function sendRecordData(uint recordId) public view returns(bool){
-        RecordData memory record = extractRecordData(getRecordDataDetialById(recordId));
-        // require(credit.uploader == msg.sender, "Only the applicant of the record can record the data.");
-        // require(record.isScored, "This record has been recorded.");
+    // 需要对 CreditData 进行查找, 时间与空间的如何选择？ 只能遍历
+    function sendRecordData(uint recordId, bool yn) public
+    returns(bool){
+        for (uint i = 0; i < applicantArray.length; i ++) {
+            for (uint j = 0; j < records[applicantArray[i]].length; j++) {
+                if (records[applicantArray[i]][j].id == recordId) {
+                    require(records[applicantArray[i]][j].owner == msg.sender, "Only the owner of the data can choose whether to send.");
+                    require(records[applicantArray[i]][j].isSent == false, "This data has been sent.");
+                    records[applicantArray[i]][j].isSent = yn;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-    function scoreRecordData(uint recordId, uint8 score) public view {
-        RecordData memory record = extractRecordData(getRecordDataDetialById(recordId));
-        require(record.applicant == msg.sender, "Only the applicant of the record can record the data.");
-        require(record.isScored, "This record has been recorded.");
-        record.score = score;
-        record.isScored = true;
+    // 机构评分 RecordData
+    function scoreRecordData(uint recordId, uint8 score) public
+    returns(bool) {
+        for (uint i = 0; i < applicantArray.length; i ++) {
+            for (uint j = 0; j < records[applicantArray[i]].length; j++) {
+                if (records[applicantArray[i]][j].id == recordId) {
+                    require(records[applicantArray[i]][j].isSent == true, "Only recieved data has been scored.");
+                    require(records[applicantArray[i]][j].applicant == msg.sender, "Only the applicant of the record can score the data.");
+                    require(records[applicantArray[i]][j].isScored == false, "This data has been recorded.");
+                    records[applicantArray[i]][j].score = score;
+                    records[applicantArray[i]][j].isScored = true;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     function extractRecordDataToBytes(RecordData memory record) private pure
     returns (bytes memory) {
-        uint _size = 130; // uint(32 bytes)*3 + unit8(1 byte)*1 + bool(1 byte)*2 + address(20 bytes)*1 = 32*3 + 1 + 1 + 32 = 130
+        uint _size = 130; // uint(32 bytes)*3 + unit8(1 byte)*1 + bool(1 byte)*2 + address(20 bytes)*2 = 32*3 + 1 + 1 + 32 * 2 = 130
         bytes memory _data = new bytes(_size);
         uint counter = 0;
         
-        // serialize address
-        uint recordAddr = uint(record.applicant);
+        // serialize applicant address
+        uint applicantAddr = uint(record.applicant);
         for (uint i = 0; i < 32; i++) {
-            _data[counter++] = byte(uint8(recordAddr>>(8*i)));
+            _data[counter++] = byte(uint8(applicantAddr>>(8*i)));
         }
+        // serialize owner address
+        uint ownerAddr = uint(record.applicant);
+        for (uint i = 0; i < 32; i++) {
+            _data[counter++] = byte(uint8(ownerAddr>>(8*i)));
+        }
+        // serialize id, creditDataId, time, score
         for (uint i = 0; i < 32; i++) {
             _data[counter++] = byte(uint8(record.id>>(8*i)));
         }
@@ -91,6 +135,7 @@ contract Record {
             _data[counter++] = byte(uint8(record.time>>(8*i)));
         }
         _data[counter++] = byte(record.score);
+        // serialize isSent and isSent
         uint8 isSS;
         if (record.isSent && record.isSent) {
             isSS = 3;
@@ -108,11 +153,14 @@ contract Record {
     function extractRecordData(bytes memory data) private pure
     returns (RecordData memory r) {
         uint counter = 0;
-        uint recordAddr = bytesToUint(data, counter);
+        // deserialize
+        uint applicantAddr = bytesToUint(data, counter);
         counter += 32;
-        
-        r.applicant = address(recordAddr);
+        r.applicant = address(applicantAddr);
+
+        uint ownerAddr = bytesToUint(data, counter);
         counter += 32;
+        r.owner = address(ownerAddr);
         
         r.id = bytesToUint(data, counter);
         counter += 32;
@@ -122,6 +170,8 @@ contract Record {
         
         r.time = bytesToUint(data, counter);
         counter += 32;
+
+        r.score = uint8(data[counter++]);
         
         uint8 isSS = uint8(data[counter]);
         if (isSS == 0) {
