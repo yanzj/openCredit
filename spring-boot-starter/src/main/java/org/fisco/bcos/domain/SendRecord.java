cbfb91111134d19cb85d76cd0977ef1a40e47c3f
YoungWilliamZ <youngwilliam.zheng@gmail.com>
2019-06-23 23:46:39 +0800

    Update for adding record

---
+++ spring-boot-starter/src/main/java/org/fisco/bcos/domain/SendRecord.java
@@ -0,0 +1,110 @@
+package org.fisco.bcos.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigInteger;
+
+/**
+ * 没
+ */
+@Entity
+public class SendRecord {
+
+    @Id
+    private BigInteger recordId;  // index
+
+    private String applicant;    // the organize want the data
+    private String uploader;     // the organize upload the data
+    private BigInteger time;
+
+    private Boolean isChecked;
+    private Boolean checkResult;  // If the record in the block chain
+
+    private Boolean isSent;       // whether the original data has been sent from the uploader
+
+    private Boolean isScored;     // whether the uploader has scored
+
+    private BigInteger score;       // the score of this record
+
+    private BigInteger creditId;
+
+    public BigInteger getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(BigInteger recordId) {
+        this.recordId = recordId;
+    }
+
+    public String getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(String applicant) {
+        this.applicant = applicant;
+    }
+
+    public String getUploader() {
+        return uploader;
+    }
+
+    public void setUploader(String uploader) {
+        this.uploader = uploader;
+    }
+
+    public BigInteger getTime() {
+        return time;
+    }
+
+    public void setTime(BigInteger time) {
+        this.time = time;
+    }
+
+    public Boolean getChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(Boolean checked) {
+        isChecked = checked;
+    }
+
+    public Boolean getCheckResult() {
+        return checkResult;
+    }
+
+    public void setCheckResult(Boolean checkResult) {
+        this.checkResult = checkResult;
+    }
+
+    public Boolean getSent() {
+        return isSent;
+    }
+
+    public void setSent(Boolean sent) {
+        isSent = sent;
+    }
+
+    public Boolean getScored() {
+        return isScored;
+    }
+
+    public void setScored(Boolean scored) {
+        isScored = scored;
+    }
+
+    public BigInteger getScore() {
+        return score;
+    }
+
+    public void setScore(BigInteger score) {
+        this.score = score;
+    }
+
+    public BigInteger getCreditId() {
+        return creditId;
+    }
+
+    public void setCreditId(BigInteger creditId) {
+        this.creditId = creditId;
+    }
+}
